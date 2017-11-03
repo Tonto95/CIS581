8be0612a1d25adac4b64458e90688178f21c9502
ahzahra <ahmed.zahra95@gmail.com>
2017-11-03 11:15:45 -0400

    Working cylindrical warping

--- cylindrical_warp.py
+++ cylindrical_warp.py
@@ -7,26 +7,45 @@ import cv2
 import numpy as np
 import math
 
-
+'''
+	Projects x,y image coordinates onto a cylinder
+		- Inputs:
+			- x,y coordinates
+			- f - focal lenght of camera
+			- x_c,y_c - center of image
+		- Outputs:
+			- x_cyl,y_cyl - cylindrical coordinate projection
+'''
 def get_cylindrical_coordinates(x,y,f,x_c,y_c):
+	# Calculate the parameters theta and h
 	theta = np.arcsin(curr_x/(np.sqrt(curr_x*curr_x + f*f)));
 	h = curr_y/(np.sqrt(curr_x*curr_x + f*f));
 
+	# Calculate the x and y cylindrical prjections
 	x_cyl = f*theta + x_c
 	y_cyl = f*h + y_c
 
 	return int(x_cyl), int(y_cyl)
 
+'''
+	Projects image onto cylinder
+		- Inputs:
+			- img: the img to be projected
+			- f - focal lenght of camera
+
+		- Outputs:
+			- img_warped: the warped image
+'''
 def cylindrical_warp(img,f):
 	h,w,p = img.shape
 
+	# Get image center coordinates
 	x_c = np.floor(w/2.0)
 	y_c = np.floor(h/2.0)
 
 	img_warped = np.zeros((int(f),int(f),3))
 
 	for i in range(w):
-
 		for j in range(h):
 			curr_x = i - x_c
 			curr_y = j - y_c 
