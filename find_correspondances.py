a0d237211663723b589fc740bdc4b0e28f6c8742
ahzahra <ahmed.zahra95@gmail.com>
2017-11-02 23:19:36 -0400

    Correspondace finding ready for testing

--- find_correspondances.py
+++ find_correspondances.py
@@ -21,7 +21,7 @@ def extract_features(img):
 	sift_obj = cv2.xfeatures2d.SIFT_create()
 	descriptors, keypoints = sift_obj.detectAndComput(img_gray, None)
 
-	return descriptors
+	return descriptors, keypoints
 
 
 '''
@@ -35,11 +35,20 @@ def extract_features(img):
 '''
 def find_correspondances(img1, img2):
 	# Extract the features from both images
-	des1 = extract_features(img1)
-	des2 = extract_features(img2)
+	des1, kp1 = extract_features(img1)
+	des2, kp2 = extract_features(img2)
 
 	# Initialize FLANN algorithm parameters to be used in the feature matching process 
 	index_params = dict(algorithm = 0, trees = 5)
 	search_params = dict(checks=50)
 
 	flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+	matches = flann.knnMatch(des1, des2, k=2)
+
+	# For testing: Display the images with correspondences 
+	img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+	cv2.imshow("correspondences", img3)
+	cv2.waitKey()
+
+	return matches
