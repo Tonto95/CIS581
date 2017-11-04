9df15f2a9f23fb3705321165902648c527c4ab1b
ahzahra <ahmed.zahra95@gmail.com>
2017-11-03 22:27:33 -0400

    Broken stitching

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from cylindrical_warp import cylindrical_warp
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from homography import homographize
+from stitch import stitch
 
 
 # Takes in a string path and the dimensions of the picture arrays
@@ -23,26 +24,37 @@ if __name__ == '__main__':
 	f = (1224*4.15)/(4.8)
 
 	warped_pics = np.zeros((pics.shape[0], int(f), int(f),3))
+	# warped_pics = pics
 
 	stitcher = cv2.createStitcher(False)
 
-	# src_pts,dst_pts = find_correspondances(pics[0], pics[1])
 
-	# H, mask = homographize(src_pts, dst_pts)
-
-	# print mask
-	for i in range(2):
+	for i in range(3):
 		warped_pics[i,:,:,:] = cylindrical_warp(pics[i],f)
 
-	result = stitcher.stitch((np.array(warped_pics[0], dtype = np.uint8), np.array(warped_pics[1], dtype = np.uint8)))
+	for i in range(2):
+		if i == 0:
+			src_pts,dst_pts = find_correspondances(warped_pics[i], warped_pics[i+1])
+			H, mask = homographize(src_pts, dst_pts)
+
+			final_img = stitch(warped_pics[i], warped_pics[i+1], H)
+		else:
+			src_pts,dst_pts = find_correspondances(final_img, warped_pics[i+1])
+			H, mask = homographize(src_pts, dst_pts)
+
+			final_img = stitch(final_img, warped_pics[i+1], H)
+			# result = stitcher.stitch((np.array(final_img, dtype = np.uint8), np.array(warped_pics[i+1], dtype = np.uint8)))
+
+
+	# result = stitcher.stitch((np.array(warped_pics[0], dtype = np.uint8), np.array(warped_pics[1], dtype = np.uint8)))
 
 
 	# matches = find_correspondances(warped_pics[0], warped_pics[1])
 
-
-	imgplot = plt.imshow(result[1])
+	imgplot = plt.imshow(final_img)
 	plt.show()
-
+	# cv2.imshow("correspondences", final_img)
+	# cv2.waitKey()
 
 
 
--- stitch.py
+++ stitch.py
@@ -1,7 +1,15 @@
+import os
+import sys
 import cv2
+import math
 import numpy as np
 
-def findDimensions(self, image, homography):
+from numpy import linalg
+
+output_dir = "/Users/Ahmed/Documents/CIS 581/CIS581Project3/Mini Project/CIS581/test_imgs"
+
+
+def findDimensions(image, homography):
 	base_p1 = np.ones(3, np.float32)
 	base_p2 = np.ones(3, np.float32)
 	base_p3 = np.ones(3, np.float32)
@@ -41,11 +49,13 @@ def findDimensions(self, image, homography):
 
 	min_x = min(0, min_x)
 	min_y = min(0, min_y)
+	
+	return (min_x, min_y, max_x, max_y)
 
-    return (min_x, min_y, max_x, max_y)
 def stitch(base_img, next_img, H): 
-    
-    (min_x, min_y, max_x, max_y) = findDimensions(next_img, H_inv)
+
+	H_inv = linalg.inv(H)
+	(min_x, min_y, max_x, max_y) = findDimensions(next_img, H_inv)
     
 	# Adjust max_x and max_y by base img size
 	max_x = max(max_x, base_img.shape[1])
@@ -73,68 +83,64 @@ def stitch(base_img, next_img, H):
 	print "New Dimensions: ", (img_w, img_h)
     
 	# Warp the new image given the homography from the old image
-	base_img_warp = cv2.warpPerspective(base_img_rgb, move_h, (img_w, img_h))
+	base_img_warp = cv2.warpPerspective(base_img, move_h, (img_w, img_h))
 	print "Warped base image"
     
 	# utils.showImage(base_img_warp, scale=(0.2, 0.2), timeout=5000)
 	# cv2.destroyAllWindows()
     
-	next_img_warp = cv2.warpPerspective(closestImage['rgb'], mod_inv_h, (img_w, img_h))
+	next_img_warp = cv2.warpPerspective(next_img, mod_inv_h, (img_w, img_h))
 	print "Warped next image"
 
     
     # Put the base image on an enlarged palette
-    enlarged_base_img = np.zeros((img_h, img_w, 3), np.uint8)
+	enlarged_base_img = np.zeros((img_h, img_w, 3), np.uint8)
     
-    print "Enlarged Image Shape: ", enlarged_base_img.shape
-    print "Base Image Shape: ", base_img_rgb.shape
+	print "Enlarged Image Shape: ", enlarged_base_img.shape
+	print "Base Image Shape: ", base_img.shape
 	print "Base Image Warp Shape: ", base_img_warp.shape
     
-	# enlarged_base_img[y:y+base_img_rgb.shape[0],x:x+base_img_rgb.shape[1]] = base_img_rgb
+	# enlarged_base_img[y:y+base_img.shape[0],x:x+base_img.shape[1]] = base_img
     # enlarged_base_img[:base_img_warp.shape[0],:base_img_warp.shape[1]] = base_img_warp
     
     # Create a mask from the warped image for constructing masked composite
-    (ret,data_map) = cv2.threshold(cv2.cvtColor(next_img_warp, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
-    
-	enlarged_base_img = cv2.add(enlarged_base_img, base_img_warp, 
-		mask=np.bitwise_not(data_map), 
-		dtype=cv2.CV_8U)
+	(ret,data_map) = cv2.threshold(cv2.cvtColor(next_img_warp.astype(np.uint8), cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
+   
+	enlarged_base_img = cv2.add(enlarged_base_img, base_img_warp, mask=np.bitwise_not(data_map), dtype=cv2.CV_8U)
     
 	# Now add the warped image
 	final_img = cv2.add(enlarged_base_img, next_img_warp, 
 		dtype=cv2.CV_8U)
     
-    # Crop off the black edges
-	final_gray = cv2.cvtColor(final_img, cv2.COLOR_BGR2GRAY)
-	_, thresh = cv2.threshold(final_gray, 1, 255, cv2.THRESH_BINARY)
-	contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-	print "Found %d contours..." % (len(contours))
+ #    # Crop off the black edges
+	# final_gray = cv2.cvtColor(final_img, cv2.COLOR_BGR2GRAY)
+	# _, thresh = cv2.threshold(final_gray, 1, 255, cv2.THRESH_BINARY)
+	# contours,_,_ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+	# print "Found %d contours..." % (len(contours))
     
-	max_area = 0
-	best_rect = (0,0,0,0)
-    
-    for cnt in contours:
-		x,y,w,h = cv2.boundingRect(cnt)
-    
-		deltaHeight = h-y
-		deltaWidth = w-x
-    
-		area = deltaHeight * deltaWidth
-    
-		if ( area > max_area and deltaHeight > 0 and deltaWidth > 0):
-			max_area = area
-			best_rect = (x,y,w,h)
-    
-		if ( max_area > 0 ):
-			print "Maximum Contour: ", max_area
-			print "Best Rectangle: ", best_rect
-    
-		final_img_crop = final_img[best_rect[1]:best_rect[1]+best_rect[3],
-			best_rect[0]:best_rect[0]+best_rect[2]]
-		final_img = final_img_crop
+	# max_area = 0
+	# best_rect = (0,0,0,0)
 
-		# Write out the current round
-		final_filename = "%s/%d.JPG" % (self.output_dir, round)
-		cv2.imwrite(final_filename, final_img)
+	# print contours
+    
+	# for cnt in contours:
+	# 	x,y,w,h = cv2.boundingRect(cnt)
+    
+	# 	deltaHeight = h-y
+	# 	deltaWidth = w-x
+    
+	# 	area = deltaHeight * deltaWidth
+    
+	# 	if ( area > max_area and deltaHeight > 0 and deltaWidth > 0):
+	# 		max_area = area
+	# 		best_rect = (x,y,w,h)
+    
+	# if ( max_area > 0 ):
+	# 	print "Maximum Contour: ", max_area
+	# 	print "Best Rectangle: ", best_rect
+    
+	# final_img_crop = final_img[best_rect[1]:best_rect[1]+best_rect[3],
+	# 	best_rect[0]:best_rect[0]+best_rect[2]]
+	# final_img = final_img_crop
 
 	return final_img
